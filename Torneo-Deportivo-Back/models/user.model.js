dc542f55c2fd970afd71dcd6710b0dac9ae6909b
soni <svega-2019050@kinal.edu.gt>
2021-06-02 20:39:00 -0600

    back user

---
+++ Torneo-Deportivo-Back/models/user.model.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = Schema({
+    name: String,
+    lastname: String,
+    username: String,
+    password: String,
+    email:String,
+    phone: Number,
+    role: String,
+    image: String,
+});
+
+module.exports = mongoose.model('user',userSchema);
